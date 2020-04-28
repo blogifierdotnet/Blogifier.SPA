e845f256ad03c7ea75796eba0752b57914e03a18
rxtur <rtur.net@gmail.com>
2020-04-28 09:15:41 -0500

    RSS feed fixed

--- src/App/Controllers/BlogController.cs
+++ src/App/Controllers/BlogController.cs
@@ -4,9 +4,14 @@ using Blogifier.Core.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.IO;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using System.Xml;
+using System.ServiceModel.Syndication;
+using System.Collections.Generic;
+using System;
 
 namespace App.Controllers
 {
@@ -15,34 +20,67 @@ namespace App.Controllers
     {
         IFeedService _ss;
         SignInManager<AppUser> _sm;
+        protected IFeedService _fs;
+        protected IDataService _ds;
 
-        public BlogController(IFeedService ss, SignInManager<AppUser> sm)
+        public BlogController(IFeedService ss, SignInManager<AppUser> sm, IDataService ds, IFeedService fs)
         {
             _ss = ss;
             _sm = sm;
+            _fs = fs;
+            _ds = ds;
         }
 
         [HttpGet("feed/{type}")]
-        public async Task Rss(string type)
+        public async Task<IActionResult> Rss(string type)
         {
-            Response.ContentType = "application/xml";
             string host = Request.Scheme + "://" + Request.Host;
+            var blog = await _ds.CustomFields.GetBlogSettings();
+            var posts = await _fs.GetEntries(type, host);
+            var items = new List<SyndicationItem>();
 
-            using (XmlWriter xmlWriter = XmlWriter.Create(Response.Body, new XmlWriterSettings() { Async = true, Indent = true }))
+            var feed = new SyndicationFeed(
+                blog.Title,
+                blog.Description,
+                new Uri(host),
+                host,
+                posts.FirstOrDefault().Published
+            );
+
+            if (posts != null && posts.Count() > 0)
             {
-                var posts = await _ss.GetEntries(type, host);
-
-                if (posts != null && posts.Count() > 0)
+                foreach (var post in posts)
                 {
-                    var lastUpdated = posts.FirstOrDefault().Published;
-                    var writer = await _ss.GetWriter(type, host, xmlWriter);
-
-                    foreach (var post in posts)
-                    {
-                        post.Description = post.Description.MdToHtml();
-                        await writer.Write(post);
-                    }
+                    var item = new SyndicationItem(
+                        post.Title,
+                        post.Description.MdToHtml(),
+                        new Uri(post.Id),
+                        post.Id,
+                        post.Published
+                    );
+                    item.PublishDate = post.Published;
+                    items.Add(item);
                 }
+            }
+            feed.Items = items;
+
+            var settings = new XmlWriterSettings
+            {
+                Encoding = Encoding.UTF8,
+                NewLineHandling = NewLineHandling.Entitize,
+                NewLineOnAttributes = true,
+                Indent = true
+            };
+
+            using (var stream = new MemoryStream())
+            {
+                using (var xmlWriter = XmlWriter.Create(stream, settings))
+                {
+                    var rssFormatter = new Rss20FeedFormatter(feed, false);
+                    rssFormatter.WriteTo(xmlWriter);
+                    xmlWriter.Flush();
+                }
+                return File(stream.ToArray(), "application/xml; charset=utf-8");
             }
         }
 
